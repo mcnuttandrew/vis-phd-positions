46f5c765e59265406e1090a75783784f84983497
Andrew Michael McNutt <mcnutt.andrew@gmail.com>
2024-07-10 14:02:06 -0700

    bump contents

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,3 +2,4 @@
 curl -L "https://docs.google.com/spreadsheets/d/e/2PACX-1vQFrUxMNxsH5Pu27PbyKdqA25aWfbUzVnOBmPQfhaxFktKGXQaNfCl_2yf8kBvzjs1dNxPb1_hezxXX/pub?output=tsv" > static/positions-24-25.tsv
 git add --a
 git commit -m 'bump contents'
+git push
